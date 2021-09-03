db373f7a634f68f57de109ce58bcba9af58a5d5b
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-09-03 13:51:16 -0500

    Homework 1

---
+++ hw1/hello.c
@@ -0,0 +1,16 @@
+// ===============================================
+// Created by Alexander Sobiek
+// LUC COMP 264, Fall 2021
+// ===============================================
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm Alex!\n");
+    printf("I have a little bit of experience with C, but interestingly enough I have never written C code using\n");
+    printf("libraries. My C experience comes from venturing down the path of learning how operating systems work at\n");
+    printf("their core, and attempting to create an extremely basic, but bootable, system. The source code\n");
+    printf("can be viewed here: https://github.com/alexsobiek/operating-system and it is my hope the classes I am\n");
+    printf("in this year will help me learn more about memory management since I ran into a few problems with that.\n");
+    return 0;
+}
