59f1d682dcea115e61af3db46869738498eb2f99
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-10-08 02:16:47 -0500

    Fixed parentheses

--- hw3/bits.c
+++ hw3/bits.c
@@ -253,7 +253,7 @@ int sign(int x) {
  */
 int rotateLeft(int x, int n) {
   // (32 + ~n + 1): number of remaining bits
-  return x << n | (x >> (32 + ~n + 1)) & ~(~0 << n);
+  return (x << n) | ((x >> (32 + ~n + 1)) & ~(~0 << n));
 }
 
 /* 
